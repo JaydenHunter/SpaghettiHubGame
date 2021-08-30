71aed0a0fd811d27a2153ab9794598c6930dfa0d
Keira <KeiKeiotix@gmail.com>
2021-08-30 17:09:16 +1000

    Actually added panel layout and beginning of code

---
+++ Assets/Scripts/PanelMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using tmpro;
+
+
+public class PanelMovement : MonoBehaviour
+{
+
+    public GameObject offScreenPosition;
+    public GameObject onScreenPosition;
+
+    //public TMP_Button openMenuButton;
+    //public TMP_Button closeMenuButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set menu to be offscreen on game start
+        gameObject.transform.position = offScreenPosition.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
