3fd6b83846b4d0f2859d5610517fe735a08c4b77
Keira <KeiKeiotix@gmail.com>
2021-08-31 17:45:17 +1000

    Panel movement added
    Connected buttons to the script
    Formated code so it is ready for ui animation

--- Assets/Scripts/PanelMovement.cs
+++ Assets/Scripts/PanelMovement.cs
@@ -10,6 +10,14 @@ public class PanelMovement : MonoBehaviour
     public GameObject offScreenPosition;
     public GameObject onScreenPosition;
 
+    //So I can hide the button
+    public GameObject buttonOpenMenu;
+
+    //Panel Transition
+    bool slideOnScreen;
+    bool slideOffScreen;
+
+
     //public TMP_Button openMenuButton;
     //public TMP_Button closeMenuButton;
 
@@ -23,6 +31,47 @@ public class PanelMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (slideOnScreen)
+        {
+            buttonOpenMenu.SetActive(false);
+
+            //Transition effect here
+            gameObject.transform.position = onScreenPosition.transform.position;
+
+            //Transition effect end here
+
+            if (gameObject.transform.position == onScreenPosition.transform.position) {
+                slideOnScreen = false;
+            }
+
+        }
+
+        if (slideOffScreen)
+        {
+            buttonOpenMenu.SetActive(true);
+
+            //Transition effect here
+            gameObject.transform.position = offScreenPosition.transform.position;
+            
+            //Transition effect end here
+
+            if (gameObject.transform.position == offScreenPosition.transform.position)
+            {
+            slideOffScreen = false;
+            }
+
+        }
 
     }
+
+    public void OnPressOpenMenu()
+    {
+        slideOnScreen = true;
+    }
+
+    public void OnPressCloseMenu()
+    {
+        slideOffScreen = true;
+    }
+
 }
