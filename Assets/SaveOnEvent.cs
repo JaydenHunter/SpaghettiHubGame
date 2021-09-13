ab05f99fa334e2224011d5868e7e96394cf977f9
Jayden <jaydenghunter@outlook.com>
2021-09-13 12:41:29 +1000

    Items only last for set time, items cost money, spawning items saves game

---
+++ Assets/SaveOnEvent.cs
@@ -0,0 +1,16 @@
+﻿//Written by Jayden Hunter
+using UnityEngine;
+
+public class SaveOnEvent : MonoBehaviour
+{
+	private SaveManager saveManager;
+
+	private void Awake()
+	{
+		saveManager = GameObject.Find("GameManager").GetComponent<SaveManager>();
+	}
+	public void SaveGame()
+	{
+		saveManager.Save();
+	}
+}
--- Assets/ToggleItem.cs
+++ Assets/ToggleItem.cs
@@ -1,16 +1,23 @@
-﻿using System.Collections;
+﻿//Written by Jayden Hunter
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToggleItem : MonoBehaviour
 {
-
+	private GameManager gameManager;
 	public GameObject item;
+	public int cost;
+	private Button button;
+
 	public bool resetPosition = true;
 	private Vector3 spawnLocation;
 	// Start is called before the first frame update
 	void Start()
 	{
+		button = GetComponent<Button>();
+		gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 		spawnLocation = item.transform.position;
 		item.SetActive(false);
 	}
@@ -18,12 +25,24 @@ public class ToggleItem : MonoBehaviour
 	public void ToggleActive()
 	{
 		bool active = item.activeInHierarchy;
-		if(!active)
+		if (!active)
 		{
+			if (gameManager.money < cost)
+				return;
+
 			item.transform.position = spawnLocation;
+			gameManager.money -= cost;
 		}
 
 		item.SetActive(!active);
+	}
+
+	private void Update()
+	{
+		if (item.activeInHierarchy || cost > gameManager.money)
+			button.interactable = false;
+		else
+			button.interactable = true;
 	}
 
 }
