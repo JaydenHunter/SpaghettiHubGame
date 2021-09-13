b2bdd95b5d5f9a7b7c52a0b62e9ac0e60af4c6a0
Keira <KeiKeiotix@gmail.com>
2021-09-13 20:41:43 +1000

    Made script to make main menu buttons work + 1
    Also fixed up panel positioning in main scene

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Coder of code: Keira
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public void GoToMainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
+
