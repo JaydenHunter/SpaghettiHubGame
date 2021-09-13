7a67be19bba9c166df47db2406d718342ae8cd93
Jayden <jaydenghunter@outlook.com>
2021-09-13 11:57:42 +1000

    Added Ball and Food Toggle, Toggle Script, Added checks to state machine

---
+++ Assets/ToggleItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleItem : MonoBehaviour
+{
+
+	public GameObject item;
+	public bool resetPosition = true;
+	private Vector3 spawnLocation;
+	// Start is called before the first frame update
+	void Start()
+	{
+		spawnLocation = item.transform.position;
+		item.SetActive(false);
+	}
+
+	public void ToggleActive()
+	{
+		bool active = item.activeInHierarchy;
+		if(!active)
+		{
+			item.transform.position = spawnLocation;
+		}
+
+		item.SetActive(!active);
+	}
+
+}
